a42c8c09fbe2e9053e8cffd0f3df76dc94e76bb8
iigmir <roc120j@gmail.com>
2020-07-01 15:51:32 +0800

    Test 3.0 feat

---
+++ src/components/RSSAPP.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        RSS
+        <button @click="increment">
+            Count is: {{ state.count }}, double is: {{ state.double }}
+        </button>
+    </div>
+</template>
+
+<script>
+import { reactive, computed } from "vue";
+
+export default {
+    setup()
+    {
+        const double = computed(() => state.count * 2);
+        const increment = () => { state.count++ };
+        const state = reactive({
+            count: 0,
+            double,
+        });
+        
+        return {
+            state,
+            increment
+        };
+    },
+}
+</script>
